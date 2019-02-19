e0698ae305cfc73de964ab42203e0933b6667702
rosilioroman <rosilioroman@gmail.com>
2019-02-18 21:40:34 -0700

    Mount SearchBar and PostContainer components to App.js. Pass the dummy-data to PostContainer as a prop.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import dummyData from './dummy-data'; //import data contained in dummy-data.js
+
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      posts: dummyData
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        Testing
+        <SearchBar />
+        <PostContainer posts={this.state.posts}/>
       </div>
     );
   }
