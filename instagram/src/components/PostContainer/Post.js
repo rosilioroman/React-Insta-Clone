1d15f1f4e49b192cb515875ec42cafe6da28d204
rosilioroman <rosilioroman@gmail.com>
2019-02-19 13:57:47 -0700

    Implement Post component. Create PostContent and PostHeader components.

--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './PostContainer.css'
 
+import PostContent from './PostContent';
+import CommentSection from '../CommentSection/CommentSection';
+
 const Post = props => {
-    return <div>Testing Post</div>
+    return (
+        <div className="post-card">
+            <PostContent content={props.post}/>
+            <CommentSection comments={props.post.comments}/>
+        </div>
+    );
+}
+
+//type check the 'post' prop being received from PostContainer.js
+Post.propTypes = {
+    post: PropTypes.shape({
+        username: PropTypes.string,
+        thumbnailUrl: PropTypes.string,
+        imageUrl: PropTypes.string,
+        likes: PropTypes.number,
+        timestamp: PropTypes.string,
+        comments: PropTypes.arrayOf(
+            PropTypes.shape({
+                username: PropTypes.string,
+                text: PropTypes.string
+            })
+        )
+    }).isRequired
 }
 
 export default Post;
