462125cab19b66ac7ab8f174f8b8e5076031046d
rosilioroman <rosilioroman@gmail.com>
2019-02-20 16:48:58 -0700

    Day 3 MVP Complete: finished all MVP goals.

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import './Login.css';
+
+import InstagramLogo from '../../images/instagram-name-logo.jpg';
 
 class Login extends Component {
     state = {
@@ -27,23 +30,30 @@ class Login extends Component {
 
     render() {
         return(
-            <>
-                <form onSubmit={this.handleLogin}>
+            <div className="login-view">
+                <form onSubmit={this.handleLogin} className="login-form">
+                    <img 
+                        src={InstagramLogo} 
+                        alt="Instagram name logo" 
+                        className="insta-logo"
+                    />
                     <input 
                         type="text" 
                         placeholder="username"
                         name="username"
                         value={this.state.username}
                         onChange={this.handleChanges}
+                        className="login-input"
                     />
                     <input 
                         type="password" 
                         placeholder="password"
                         name="password"
+                        className="login-input"
                     />
-                    <button type="submit">Log In</button>
+                    <button type="submit" className="login-button">Log In</button>
                 </form>
-            </>
+            </div>
         );
     }
 }
