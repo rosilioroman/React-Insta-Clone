f6c1c9a1919a1e9c4bd80cf03ca5225ea31836d7
rosilioroman <rosilioroman@gmail.com>
2019-02-20 16:19:50 -0700

    MVP Day 3 Goal Complete: finished implementing login functionality.

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+    state = {
+        username: ""
+    }
+
+    handleChanges = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleLogin = e => {
+        //prevent default behavior of <form> element
+        e.preventDefault();
+
+        //check that the user input a username
+        if(!this.state.username) alert('Username field cannot be empty'); 
+
+        //save the username in all lowercase in localStorage
+        localStorage.setItem('username', this.state.username.toLowerCase());
+
+        //un-mount and remount the component
+        window.location.reload();
+    }
+
     render() {
         return(
             <>
-                <form>
-                    <input type="text" placeholder="username"/>
-                    <input type="password" placeholder="password"/>
+                <form onSubmit={this.handleLogin}>
+                    <input 
+                        type="text" 
+                        placeholder="username"
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.handleChanges}
+                    />
+                    <input 
+                        type="password" 
+                        placeholder="password"
+                        name="password"
+                    />
                     <button type="submit">Log In</button>
                 </form>
             </>
--- instagram/src/components/authentication/authenticate.js
+++ instagram/src/components/authentication/authenticate.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 
 const authenticate = App => Login => (
     class extends Component {
-        render() {
-            if(false) {
-                return <App />
-            }
+        state = {
+            isLoggedIn: false
+        }
 
+        //when the component mounts, check local storage for a 'username' key
+        componentDidMount() {
+            if(localStorage.getItem('username')) {
+                this.setState({ isLoggedIn: true });
+            }
+        }
+
+        render() {
+            //if isLoggedIn is true, return the App component
+            if(this.state.isLoggedIn) return <App />
+
+            //by default, return the Login component
             return <Login />
         }
     }
